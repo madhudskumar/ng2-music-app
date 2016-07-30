467393523e32912bab716266c8f2b09de282c15f
madhudskumar <madhudskumar@live.in>
2016-07-30 22:05:43 +0530

    added country service

--- app/common/country.service.ts
+++ app/common/country.service.ts
@@ -1,2 +1,29 @@
 import { iCountry } from '../models/country/country.interface'
 import { Country } from '../models/country/country.class'
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Rx";
+
+@Injectable()
+export class counttryService{
+    constructor(
+        private _http:Http
+    ){}
+
+    getCountries():Observable{
+        return new Observable(observable => {
+            this._http.get("https://restcountries.eu/rest/v1/all")
+                .map(res => {
+                    res.json();
+                    var countries:Array<iCountry> = [];
+                    res.forEach(data => {
+                        countries.push(new Country(data.name, data.alpha2Code));
+                    })
+                    return countries
+                })
+                .subscribe(res => {
+                    observable.next(res);
+                })
+        })
+    }
+}
